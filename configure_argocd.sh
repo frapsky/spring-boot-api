d4d5f0662b875065b21f06317a7aadaa6af1bfc5
frapsky <jakubszostk@gmail.com>
2025-04-15 12:41:30 +0200

    adjusted argocd configuration, allowed resources and namespaces,

--- configure_argocd.sh
+++ configure_argocd.sh
@@ -4,7 +4,7 @@ set -e
 
 ARGOCD_NAMESPACE="argocd"
 ARGOCD_PROJECT_NAME="spring-boot-api-apps"
-ARGOCD_PASSWORD="XAX3El7Wb7Ps6NqK"
+ARGOCD_PASSWORD=""
 
 REPO_URL="https://github.com/frapsky/spring-boot-api.git"
 DEST_SERVER="https://kubernetes.default.svc"
@@ -15,8 +15,8 @@ argocd proj create "$ARGOCD_PROJECT_NAME" \
   --description "Project for Spring Boot API Applications" \
   --src "$REPO_URL" \
   --dest "$DEST_SERVER,*" \
+  --allow-namespaced-resource "*" "*" \
+  --allow-cluster-resource "*" "*" \
   --upsert || error "Failed to create/update Argo CD project."
-
-#argocd proj allow-namespace-resource "$ARGOCD_PROJECT_NAME" "*/*" || error "Failed to set namespaced resource permissions for project."
 
 kubectl apply -f argocd/applicationset.yaml -n argocd
